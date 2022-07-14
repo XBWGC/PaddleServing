da7690259ff5d8a9b5e811096c798d2e82bc8e1b
XBWGC <xiaobingw@graphcore.ai>
2022-07-14 14:10:06 +0800

    enable debug with vscode

--- tools/start_docker.sh
+++ tools/start_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run --rm \
+docker run \
+  --name paddle_serving \
   -v `pwd`:/Serving \
   -p 8080:8080 \
   -it registry.baidubce.com/paddlepaddle/serving:0.9.0-devel \
---
+++ tools/stop_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker stop paddle_serving && docker rm paddle_serving
